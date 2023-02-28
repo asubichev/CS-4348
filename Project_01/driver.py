277f67d18f5b5a534340d97f17262bec992abd4e
asubichev <asubichev@gmail.com>
2023-02-28 15:07:39 -0600

    processes made w/ pipes

--- Project_01/driver.py
+++ Project_01/driver.py
@@ -1,14 +1,21 @@
 import sys
+from subprocess import Popen, PIPE
 
 def main():
     if len(sys.argv) > 2:
         raise Exception('Only argument should be log file')
     ff = sys.argv[1]
 
+    #make ps for logger program
+    logps = Popen(['python', 'logger.py'], stdout=PIPE, stdin=PIPE, encoding='utf8')
+    #make ps for encryption program
+    encps = Popen(['python', 'encrypt.py'], stdout=PIPE, stdin=PIPE, encoding='utf8')
+
     history = []
     print('Please enter a command: ', end='')
     for line in sys.stdin:
         if line == 'quit':
+            #send QUIT to enc & log
             break
         elif line == 'password':
             print()
@@ -22,10 +29,7 @@ def main():
             print('Invalid Commmand')
         print()
 
-    #make ps for logger program
-    #make ps for encryption program
-
-    print('Exiting now --')
+    print('Program Quit')
 
 if __name__ == '__main__':
     main()
