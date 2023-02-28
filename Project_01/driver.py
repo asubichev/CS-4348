34a0ce5f6ca8cf2bc94c6986ed0be396f51923e8
asubichev <asubichev@gmail.com>
2023-02-28 14:47:29 -0600

    input control

--- Project_01/driver.py
+++ Project_01/driver.py
@@ -5,6 +5,23 @@ def main():
         raise Exception('Only argument should be log file')
     ff = sys.argv[1]
 
+    history = []
+    print('Please enter a command: ', end='')
+    for line in sys.stdin:
+        if line == 'quit':
+            break
+        elif line == 'password':
+            print()
+        elif line == 'encrypt':
+            print()
+        elif line == 'decrypt':
+            print()
+        elif line == 'history':
+            print()
+        else:
+            print('Invalid Commmand')
+        print()
+
     #make ps for logger program
     #make ps for encryption program
 
