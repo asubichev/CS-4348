22e3eac96071ed1d27f41e8ab22019d0d0b02fd5
asubichev <asubichev@gmail.com>
2023-02-28 02:54:48 -0600

    decryption fnc done, result status started

--- Project_01/encrypt.py
+++ Project_01/encrypt.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def main():
     passkey = ''
@@ -9,18 +10,26 @@ def main():
         cmd = msg[0]
         if cmd == 'QUIT':
             break
-        #TODO: convert arg to all uppercase
-        arg = msg[1]
+        arg = msg[1].upper()
+
+        result = [None, None]
         if cmd == 'PASSKEY':
-            passkey = msg[1]
+            if re.matches('[A-Z]*', arg):
+                passkey = msg[1]
+                result = [0, '']
+            else:
+                result = [-1, '']
         elif cmd == 'ENCRYPT':
-            encrypt(msg[1], passkey)
+            result = encrypt(arg, passkey)
         elif cmd == 'DECRYPT':
-            decrypt(msg[1], passkey)
+            result = decrypt(arg, passkey)
     #program end
 
 #will return encrypted key, or return error
+#returns list with exit code and string, where arr[0] == -1 is an error, and arr[0] == 0 is success
 def encrypt(msg, key):
+    if key == '': return [-1, '']
+
     #first, make key as long as msg
     #next, iterate through msg and key together to find enc string
     ogkeylen = len(key)
@@ -38,12 +47,30 @@ def encrypt(msg, key):
         newchar = chr(newpos + 65)
         outputstr += newchar
     
-    return outputstr
+    return [0, outputstr]
 
 
 #will return decrypted key, or return error
-def decrypt():
-    print()
+def decrypt(msg, key):
+    if key == '': return [-1, '']
+
+    #make key as long as message again
+    ogkeylen = len(key)
+    if len(msg) > ogkeylen:
+        key = (len(msg) // ogkeylen) * key
+        for kk in range(len(msg) - len(key)):
+            key += key[kk]
+
+    #reverse steps of encryption
+    outputstr = ''
+    for i in range(len(msg)):
+        nmshift = ord(key[i]) - 65
+        newpos = ord(msg[i]) - 65 - nmshift
+        if newpos < 0: newpos += 26
+        newchar = chr(newpos + 65)
+        outputstr += newchar
+
+    return [0, outputstr]
 
 if __name__ == "__main__":
     main()
